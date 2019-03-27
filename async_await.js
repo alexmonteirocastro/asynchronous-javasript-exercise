c77e46cb0cbf1ddf660d42ec2a2147bc480d184d
alexmonteirocastro <alex.monteiro@outlook.pt>
2019-03-27 21:30:42 +0100

    refactor HTTP functions to use built-in JSON parser

--- async_await.js
+++ async_await.js
@@ -1,15 +1,17 @@
 const rp = require('request-promise')
 const BASE_URL = 'https://swapi.co/api/'
+const options = {
+    uri: `${BASE_URL}people/1`,
+    json: true 
+}
 
 async function getCharacterVehicleNames(){
     console.log('Fetching User Data...')
-    const characterData = await rp(`${BASE_URL}people/1`)
-    const characterVehicles = JSON.parse(characterData).vehicles
+    const characterData = await rp(options)
     console.log('Fetching Vehicles data...')
-    characterVehicles.map(async (vehicle, index) => {
-        const vehicleDetails = await rp(vehicle)
-        const vehicleName = JSON.parse(vehicleDetails).name
-        console.log(`Vehicle ${index + 1}: ${vehicleName}`)
+   characterData.vehicles.map(async (vehicle, index) => {
+        const vehicleDetails = await rp({ uri: vehicle, json: true })
+        console.log(`Vehicle ${index + 1}: ${vehicleDetails.name}`)
     })
 }
 
--- promises.js
+++ promises.js
@@ -1,16 +1,19 @@
 const rp = require('request-promise')
 const BASE_URL = 'https://swapi.co/api/'
+const options = {
+    uri: `${BASE_URL}people/1`,
+    json: true 
+}
 
 function fetchCharacterVehicleNames(endPoint){
     console.log('Fetching character data...')
-    rp(`${BASE_URL}people/1`).then(response => fetchVehicles(JSON.parse(response)))
+    rp(options).then(response => fetchVehicles(response))
 } 
 
 function fetchVehicles(character){
     console.log('Fetching vehicle data...')
     character.vehicles.map((vehicle, index) => {
-        rp(vehicle).then(response => {
-            const vehicleData = JSON.parse(response)
+        rp({ uri: vehicle, json: true }).then(vehicleData => {
             console.log(`Vehicle ${index + 1}: ${vehicleData.name}`)
         })
     })
