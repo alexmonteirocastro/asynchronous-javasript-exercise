4474bbf3e85fa290ae93c763e50217612b62de3c
alexmonteirocastro <alex.monteiro@outlook.pt>
2019-03-27 19:32:38 +0100

    First files and Callback approach

---
+++ README.md
@@ -0,0 +1,16 @@
+# Asynchronous JavaScript
+
+This is the first homework assignment for the NodeJS nigths Brno by STRV, 
+
+## Exercise specs
+
+Can be found [here](https://github.com/strvcom/nodejs-nights-2019-brno/tree/master/lectures/01-javascript).
+
+### Brief description
+
+This module should fetch data from a third-party RESTful API and print it to the console. 
+Three methods were used. 
+
+1. CallBack
+2. Promises
+3. Async/await
---
+++ index.js
@@ -0,0 +1,26 @@
+var request = require('request');
+
+const BASE_URL = 'https://swapi.co/api/'
+
+function getCharacterDetails(route){
+    console.log('Fetching character data...')
+    request(route, function (error, response) {
+        fetchVehicles(JSON.parse(response.body))
+    });
+}
+
+function fetchVehicles(character){
+    console.log('Fetching vehicle data...')
+    printVehicleNames(character.vehicles)   
+}
+
+function printVehicleNames(vehiclesArray){
+    vehiclesArray.map((vehicle, index) => {
+        request(vehicle, function (error, response) {
+            const vehicleData = JSON.parse(response.body)
+            console.log(`Vehicle ${index}: ${vehicleData.name}`)
+        });
+    })    
+}
+
+getCharacterDetails(`${BASE_URL}people/1`)
