72da1a97a90d04a2129df6c69512027358320cd7
Alex Castro <alex.monteiro@outlook.pt>
2019-03-27 22:30:20 +0100

    Refactored async / await module using Promise.all

--- async_await.js
+++ async_await.js
@@ -1,18 +1,26 @@
 const rp = require('request-promise')
 const BASE_URL = 'https://swapi.co/api/'
 const options = {
-    uri: `${BASE_URL}people/1`,
-    json: true 
+  uri: `${BASE_URL}people/1`,
+  json: true
 }
 
-async function getCharacterVehicleNames(){
-    console.log('Fetching User Data...')
-    const characterData = await rp(options)
-    console.log('Fetching Vehicles data...')
-   characterData.vehicles.map(async (vehicle, index) => {
-        const vehicleDetails = await rp({ uri: vehicle, json: true })
-        console.log(`Vehicle ${index + 1}: ${vehicleDetails.name}`)
+async function getCharacterVehicleNames() {
+  console.log('Fetching User Data...')
+  const characterData = await rp(options)
+  const vehicles = characterData.vehicles
+  console.log('Fetching Vehicles data...')
+  Promise.all(
+    vehicles.map(async vehicleUrl => {
+      const vehicleDetails = await rp({
+        uri: vehicleUrl,
+        json: true
+      })
+      return vehicleDetails
     })
+  ).then(vehicleData => {
+    vehicleData.map((vehicle, index) => console.log(`Vehicle ${index + 1}: ${vehicle.name}`))
+  })
 }
 
 getCharacterVehicleNames()
